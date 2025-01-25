01b3b121c45340084c6f2fa2a176a48137cf4c08
Ihsan Saif <ihsansaifedwardion@gmail.com>
2025-01-25 23:26:10 +0500

    Add files via upload

---
+++ intro.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+int number;
+float point;
+char letter = 'a';
+string word = "hello";
+cout<<"Enter value of int: ";
+cin>> number;
+cout <<"enter the value of float: ";
+cin>>point;
+float result=number+ point;
+
+cout<<"The result of number + point is: "<<result;
+cout<<"The value of letter is: "<<letter;
+cout<<"The value of word is: "<<word;
+
+
+ return 0;
+}
